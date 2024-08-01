d9e8f50a858cf60f8acfe54eaeb173cc26603168
100791 <100791@MACTECH.LOC>
2024-08-01 17:03:44 +0530

    WeatherApp

---
+++ lib/utils/const.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const String fontFamily="Poppins";
+
+
+//sized boxes
+const kheight10=SizedBox(
+  height: 10,
+);
+
+const kheight15=SizedBox(
+  height: 15,
+);
+
+const kheight20=SizedBox(
+  height: 20,
+);
+
+const kheight25=SizedBox(
+  height: 25,
+);
+//widths
+
+const kwidth5 = SizedBox(
+  width: 5,
+);
+const kwidth10 = SizedBox(
+  width: 10,
+);
+const kwidth15 = SizedBox(
+  width: 15,
+);
+const kwidth20 = SizedBox(
+  width: 20,
+);
+const kwidth25 = SizedBox(
+  width: 25,
+);
+
+
